73f8b856b4a95a70af753e2965fcad9fd2b6a525
Kumar Ojas <44812427+OJASKUMAR@users.noreply.github.com>
2019-08-27 23:49:24 +0530

    Update Mariolev1.py

--- Mariolev1.py
+++ Mariolev1.py
@@ -1,10 +1,4 @@
-#!/usr/bin/env python
-__author__ = 'justinarmstrong'
 
-"""
-This is an attempt to recreate the first level of
-Super Mario Bros for the NES.
-"""
 
 import sys
 import pygame as pg
